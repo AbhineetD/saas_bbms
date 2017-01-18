7452817732ff21b42ff9ad49835fba234462816a
Abhineet Deshpande <abhineet.d@gmail.com>
2017-01-18 13:34:13 +0000

    Added form validation to model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 class Contact < ActiveRecord::Base
-  
+  validates :name, presence: true  
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :dob, presence: true
+  validates :fathers_name, presence: true
+  #validates :occupation, presence: true
+  #validates :organisation, presence: true
+  validates :address, presence: true
+  #validates :email, presence: true
+  validates :phone, presence: true
   
 end
