aea98963682bafb4ee3e7150975286ec1b40707a
Abhineet Deshpande <abhineet.d@gmail.com>
2017-02-09 18:56:40 +0000

    Whitelisting devise sign up form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # Whitelist form fields if coming from Devise sign up form
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+    end
 end
